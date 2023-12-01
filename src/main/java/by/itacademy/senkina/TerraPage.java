7bae71e606ef233843aa1e51d9062894f2af5b98
Yuliya-senkina <juliyasenkina@gmail.com>
2023-12-01 23:41:26 +0300

    Initial commit

--- src/main/java/by/itacademy/senkina/TerraPage.java
+++ src/main/java/by/itacademy/senkina/TerraPage.java
@@ -4,14 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class TerraPage {
-    public String inputButtonClose = "//button[@class=\"close-icon\"]";
+    public String inputButtonClose = "//button[@class='close-icon']";
     public String inputButtonPizza = "//li[@class='menu-navigation__item']/a[@href='/catalog/picca']";
     public String inputButtonInBasket = "//button[@data-id='364']";
     public String inputButtonBar = "//li[@class='menu-navigation__item']/a[@href='/catalog/bar']";
-    public String inputButtonAddDrink = "//button[@data-id=\"2219\"]";
-    public String inputButtonBasket = "//div[@class=\"basket__btn-top basket__top basket__btn-top--sm\"]";
-    public String textNamePizza = "//li[@id=\"basket-el-0\"]//div[@class=\"basket__products-item-name\"]";
-    public String textNameDrink = "//li[@id=\"basket-el-1\"]//div[@class=\"basket__products-item-name\"]";
+    public String inputButtonAddDrink = "//button[@data-id='2219']";
+    public String inputButtonBasket = "//div[@class='basket__btn-top basket__top basket__btn-top--sm']";
+    public String textNamePizza = "//li[@id='basket-el-0']//div[@class='basket__products-item-name']";
+    public String textNameDrink = "//li[@id='basket-el-1']//div[@class='basket__products-item-name']";
 
     WebDriver driver;
 
